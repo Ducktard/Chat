261c054b7f8897d8be432b0f696e56cb717f94b4
wmc035 <wmc035@DVS-APO.wiso.ad.uni-koeln.de>
2016-12-07 12:59:39 +0100

    1

--- Chat/middleware/Server/Server.java
+++ Chat/middleware/Server/Server.java
@@ -1,12 +1,22 @@
 
 public class Server {
 
-	privat boolean active = false;
+	//Variablen
+	private boolean isActive = false;
+	private ArrayList<Session> sessions = new ArrayList<Session>();
+	//Konstanten
+	final private int MAX_SESSIONS = 10;
 	
+	
+	//Serverschleife
 	public serverloop(){
-		
+		while(this.isActive && this.sessions this.MAX_SESSIONS){
+			
+		}
 	}
+	
+	//Server Start
 	public start(){
-		while()
+		
 	}
 }
