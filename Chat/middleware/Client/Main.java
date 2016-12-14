4ea565fe69376216ece727dead8a1272801494b5
wmc035 <wmc035@DVS-APO.wiso.ad.uni-koeln.de>
2016-12-14 08:35:28 +0100

    2

--- Chat/middleware/Client/Main.java
+++ Chat/middleware/Client/Main.java
@@ -2,6 +2,9 @@ package Client;
 
 public class Main {
 
-	
+	private static void main (String[]args){
+		
+		Client client = new Client();
+	}
 	
 }
--- Chat/middleware/Server/Server.java
+++ Chat/middleware/Server/Server.java
@@ -6,22 +6,23 @@ import Server.Session;
 
 public class Server {
 
-	//variables
+	// variables
 	private boolean isActive = false;
 	private ArrayList<Session> sessions = new ArrayList<Session>();
-	//Konstanten
+	// Konstanten
 	final private int MAX_SESSIONS = 10;
-	
-	
-	//Serverloop
-	public void serverloop(){
-		while(this.isActive && this.sessions.size() < this.MAX_SESSIONS){
-			
+
+	// Server Start
+
+	public  synchronized void start(){
+		this.isActive = true;
+	}
+
+	// Serverloop
+	public void serverloop() {
+		while (this.isActive && this.sessions.size() < this.MAX_SESSIONS) {
+
 		}
 	}
-	
-	//Server Start
-	public void start(){
-		
-	}
+
 }
